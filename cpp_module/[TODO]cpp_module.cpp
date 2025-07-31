a28da8ade115347bef09fb56dba285c215f5db06
Shirzart Enwer <shirzartenwer@gmail.com>
2025-07-31 18:20:22 +0200

    doc: first draft of TODOs in C++

--- cpp_module/[TODO]cpp_module.cpp
+++ cpp_module/[TODO]cpp_module.cpp
@@ -1,5 +1,4 @@
 // cpp_module.cpp
-
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <opencv2/opencv.hpp>
@@ -9,9 +8,21 @@ namespace py = pybind11;
 py::array_t<uint8_t> blur_largest_shape_in_rect(
     py::array_t<uint8_t> input_array,
     py::tuple rect_tuple,
-    int blur_kernel = 15)
+    int blur_kernel = 15,
+    uint16_t img_width)
 {
     // TODO: Implement this function
+    // Construct the image using the input_arry and buffer info
+    // Crop the data
+    
+    // Normalize the ROI 
+    // Find the largest shape using contours - need to research the most effective algo
+
+    // blur origianl image with the gaussian blur using blur kernel
+
+    // return it
+
+
     // [candidate to fill]
     
     throw std::runtime_error("Function not yet implemented");
